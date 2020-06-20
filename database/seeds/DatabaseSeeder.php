c7d7e5fb614cc3ab95e796dc72dbe33f1f562e40
smlsunxie <smlsun@gmail.com>
2020-06-20 16:40:43 +0800

    add jwt support

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,10 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UserSeeder::class);
+        \App\User::create([
+            "name" => "admin",
+            "email" => "admin@example.com",
+            "password"=> bcrypt('admin')
+        ]);
     }
 }
